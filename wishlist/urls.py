80cca2495b519dd653e5f19a029146bd3b8cfb1d
symphonycellis <symphony.cellis@ui.ac.id>
2022-09-15 13:23:08 +0700

    pengerjaan lab 2

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from wishlist.views import show_wishlist
-
+from wishlist.views import show_xml #sesuaikan dengan nama fungsi yang dibuat
+from wishlist.views import show_json #sesuaikan dengan nama fungsi yang dibuat
+from wishlist.views import show_json_by_id #sesuaikan dengan nama fungsi yang dibuat
 app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
+    path('xml/', show_xml, name='show_xml'), #sesuaikan dengan nama fungsi yang dibuat
+    path('json/', show_json, name='show_json'), #sesuaikan dengan nama fungsi yang dibuat
+    path('json/<int:id>', show_json_by_id, name='show_json_by_id'), #sesuaikan dengan nama fungsi yang dibuat
 ]
